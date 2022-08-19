c2e0db0d06d3295de22c7634e3dc3b361563b1cf
Desmond Madden <desmond.madden@hotmail.co.uk>
2022-08-19 21:42:44 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Who Wants To Be A Millionaire?
+
+Simple game created whilst learning Golang
+
+To run the code:
+```
+go run main.go
+```
+
+## To do
+- Shuffle questions and answers
+- Lifelines
+- More questions
+- Gracefully handle errors
+- Add user prompts
+- Fancy text formatting?
+- Unit tests
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	alive := true
+	questNum := 0
+	questions := getQuestions()
+	fmt.Println(questions)
+	println("WELCOME TO WHO WANTS TO BE A MILLIONAIRE")
+	for alive {
+		if questNum == 15 {
+			println("YOU HAVE WON!")
+			alive = false
+		}
+		println("QUESTION NUMBER: ", questNum+1, "/15")
+		qs := questions[questNum]
+		var inp int
+		println(qs[0])
+		println("1.", qs[1])
+		println("2.", qs[2])
+		println("3.", qs[3])
+		println("4.", qs[4])
+		println("Please enter the answer: 1..4")
+		fmt.Scan(&inp)
+
+		if qs[inp] == qs[5] {
+			println("YOU'RE CORRECT!")
+			questNum++
+		} else {
+			println("YOU'RE WRONG")
+			alive = false
+		}
+	}
+}
+
+func getQuestions() [][]string {
+	bs, err := ioutil.ReadFile("questions")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return formatSlice(s)
+}
+
+func formatSlice(questions []string) [][]string {
+	qSlice := [][]string{}
+	for i := 0; i < len(questions); i++ {
+		if i%6 == 0 {
+			qSlice = append(qSlice, questions[i:i+6])
+		}
+	}
+	return qSlice
+}
+
+/**
+To do:
+- Shuffle questions and answers
+- Add lifelines
+- Add more questions
+- Add error handling when typing numbers
+- Add are you sure prompts
+- Fancy text formatting?
+*/
